5c6a4838166b39897d02d54e847b329c02628a86
Valdir <valdir@substractum.com.br>
2017-12-06 12:43:01 -0200

    Criar matriz

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,36 +1,66 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javax.xml.soap.Text;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class Main extends Application {
 
+    int SIZE = 10;
+    int length = SIZE;
+    int width = SIZE;
+
+    int rowIndex = 0;
+    int columnIndex = 0;
 
 
     @Override
     public void start(Stage primaryStage) {
 
-        int SIZE = 10;
-        int length = SIZE;
-        int width = SIZE;
-
         GridPane root = new GridPane();
 
+
+        addChildrens(rowIndex, columnIndex, root);
+
+
+        Scene scene = new Scene(root, 500, 500);
+        primaryStage.setTitle("Exemplo 1");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
+            if(keyEvent.getCode().equals(KeyCode.RIGHT)){
+                moveFront(root);
+            } else if(keyEvent.getCode().equals(KeyCode.UP)){
+                moveUp(root);
+            }
+
+        });
+
+
+    }
+
+    private void moveUp(GridPane root){
+        System.out.println("Move up");
+        rowIndex++;
+        addChildrens(rowIndex, columnIndex, root);
+    }
+
+    private void moveFront(GridPane root){
+        System.out.println("Move front");
+        columnIndex++;
+        addChildrens(rowIndex, columnIndex, root);
+    }
+
+    private void addChildrens(int rowIndex, int columnIndex, GridPane root) {
         for(int y = 0; y < length; y++){
-            for(int x = 0; x < width; x++){
+            for(int x = 0; x < width; x++) {
+
 
                 TextField tf = new TextField();
                 tf.setPrefHeight(50);
@@ -39,7 +69,7 @@ public class Main extends Application {
                 tf.setEditable(false);
 
 
-                if(y == 0 && x == 0){
+                if (y == rowIndex && x == columnIndex) {
                     tf.setText("A");
                 } else {
                     tf.setText("");
@@ -51,28 +81,6 @@ public class Main extends Application {
                 root.getChildren().add(tf);
             }
         }
-
-        Scene scene = new Scene(root, 500, 500);
-        primaryStage.setTitle("Exemplo 1");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
-
-            if(keyEvent.getCode().equals(KeyCode.RIGHT)){
-                System.out.println("Move front");
-            } else if(keyEvent.getCode().equals(KeyCode.UP)){
-                System.out.println("Move up");
-            }
-
-
-        });
-
-
-
-
-
-
     }
 
     public static void main(String[] args) {
