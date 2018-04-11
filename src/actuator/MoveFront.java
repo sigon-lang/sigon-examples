5298948a3f3678c97ad36af2b0429c10ded039ce
Valdir Luiz <arnholdvaldir@gmail.com>
2018-04-11 19:29:36 -0300

    testes

--- src/actuator/MoveFront.java
+++ src/actuator/MoveFront.java
@@ -3,6 +3,8 @@ package actuator;
 
 import br.ufsc.ine.agent.context.beliefs.BeliefsContextService;
 import br.ufsc.ine.agent.context.communication.Actuator;
+import br.ufsc.ine.agent.context.desires.DesiresContextService;
+import br.ufsc.ine.agent.context.intentions.IntentionsContextService;
 import javafx.application.Platform;
 import sample.Main;
 
@@ -11,6 +13,9 @@ import java.util.List;
 public class MoveFront  extends Actuator {
 
     public void act(List<String> list) {
+
+
+
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
